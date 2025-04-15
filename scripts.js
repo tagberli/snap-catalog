1bf38d12a545986c48231cea6b4e7dfd8ea346c1
tagberliyev <sunrisesimcoding@gmail.com>
2025-04-15 03:27:42 -0700

    Pre-full-dataset version

--- scripts.js
+++ scripts.js
@@ -23,75 +23,165 @@
  *
  */
 
-const FRESH_PRINCE_URL =
-  "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL =
-  "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL =
-  "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
 
-// This is an array of strings (TV show titles)
-let titles = [
-  "Fresh Prince of Bel Air",
-  "Curb Your Enthusiasm",
-  "East Los High",
+// All the data is taken from the popular website named "myanimelist.net"
+// here is the dataset I made myself it is pretty long :(
+const animeList = [
+  {
+    title: "Attack on Titan",
+    image: "https://cdn.myanimelist.net/images/anime/1517/100633.jpg",
+    gif: "https://media1.tenor.com/m/CYYG2F1rZxcAAAAd/ao-t-attack-on-titan.gif",
+    rating: 9.05,
+    popularity: 2431574,
+    genre: "Action, Drama, Fantasy",
+    total_episodes: 94
+  },
+  {
+    title: "Sousou no Frieren",
+    image: "https://cdn.myanimelist.net/images/anime/1015/138006.jpg",
+    gif: "https://media1.tenor.com/m/mgN_JjdTDzYAAAAd/anime-spell.gif",
+    rating: 9.30,
+    popularity: 1089020, // number of members who added it to their watchlist
+    genre: "Action, Adventure, Shounen",
+    total_episodes: 28
+  },
+  // Add more entries as needed
 ];
-// Your final submission should have much more data than this, and
-// you should use more than just an array of strings to store it all.
 
-// This function adds cards the page to display the data in the array
-function showCards() {
-  const cardContainer = document.getElementById("card-container");
-  cardContainer.innerHTML = "";
-  const templateCard = document.querySelector(".card");
 
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
 
-    // This part of the code doesn't scale very well! After you add your
-    // own data, you'll need to do something totally different here.
-    let imageURL = "";
-    if (i == 0) {
-      imageURL = FRESH_PRINCE_URL;
-    } else if (i == 1) {
-      imageURL = CURB_POSTER_URL;
-    } else if (i == 2) {
-      imageURL = EAST_LOS_HIGH_POSTER_URL;
-    }
 
-    const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
-    cardContainer.appendChild(nextCard); // Add new card to the container
+
+
+// START OF CATALOG OPERATIONS
+let myWatchlist = [];
+let currentList = 'all';
+
+// WATCHLIST OPERATIONS
+function switchList(listType) {
+  currentList = listType;
+
+  const header = document.querySelector("header");
+  const pageTitle = document.getElementById("page-title");
+
+  if (listType === 'all') {
+    pageTitle.innerText = "Anime Catalog";
+    header.style.backgroundColor = "#1F1F1F"; //
+  } else {
+    pageTitle.innerText = "My Watchlist";
+    header.style.backgroundColor = "#4A90E2";
   }
+
+  displayList();
 }
 
-function editCardContent(card, newTitle, newImageURL) {
-  card.style.display = "block";
-
-  const cardHeader = card.querySelector("h2");
-  cardHeader.textContent = newTitle;
-
-  const cardImage = card.querySelector("img");
-  cardImage.src = newImageURL;
-  cardImage.alt = newTitle + " Poster";
-
-  // You can use console.log to help you debug!
-  // View the output by right clicking on your website,
-  // select "Inspect", then click on the "Console" tab
-  console.log("new card:", newTitle, "- html: ", card);
+function addToWatchlist(animeTitle) {
+  const anime = animeList.find(a => a.title === animeTitle);
+  if (!myWatchlist.some(a => a.title === animeTitle)) {
+    myWatchlist.push(anime);
+  }
+  displayList();
 }
 
-// This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
-
-function quoteAlert() {
-  console.log("Button Clicked!");
-  alert(
-    "I guess I can kiss heaven goodbye, because it got to be a sin to look this good!"
-  );
+function removeFromWatchlist(animeTitle) {
+  myWatchlist = myWatchlist.filter(anime => anime.title !== animeTitle);
+  displayList();
 }
 
-function removeLastCard() {
-  titles.pop(); // Remove last item in titles array
-  showCards(); // Call showCards again to refresh
+function getCurrentList() {
+  return currentList === 'all' ? [...animeList] : [...myWatchlist];
 }
+
+
+
+// SORTING OPERATION
+function sortList(byRating) {
+  const list = getCurrentList();
+
+  if (byRating == true){
+    list.sort((a, b) => b.rating - a.rating);
+  } else{
+    list.sort((a, b) => b.popularity - a.popularity);
+  }
+  displayList(list);
+}
+
+// FILTERING OPERATION
+function filterByGenre(genre) {
+  const list = getCurrentList();
+  const filtered = genre ? list.filter(anime => anime.genre.includes(genre)) : list;
+  displayList(filtered);
+}
+
+
+
+// DISPLAY HELPER FUNCTION
+function displayList(data = null) {
+  const container = document.getElementById('anime-list');
+  container.innerHTML = '';
+  const list = data || getCurrentList();
+
+  list.forEach(anime => {
+    const card = document.createElement('div');
+    card.className = 'anime-card';
+
+    const inWatchlist = myWatchlist.some(a => a.title === anime.title);
+    const buttonText = inWatchlist ? 'Remove from Watchlist' : 'Add to Watchlist';
+    const buttonClass = inWatchlist ? 'remove' : '';
+    const buttonAction = inWatchlist ? `removeFromWatchlist('${anime.title}')` : `addToWatchlist('${anime.title}')`;
+
+    // Create image element
+    const img = document.createElement('img');
+    img.src = anime.image; // Default image
+    img.alt = anime.title;
+    img.className = 'anime-img';
+
+    // Hover effect to switch image to GIF
+    img.addEventListener('mouseenter', () => {
+      img.src = anime.gif; // Replace with GIF on hover
+    });
+
+    img.addEventListener('mouseleave', () => {
+      img.src = anime.image; // Revert to original image when hover ends
+    });
+
+    // Insert title
+    const title = document.createElement('h3');
+    title.innerText = anime.title;
+
+    // Add other anime details
+    const rating = document.createElement('p');
+    rating.innerHTML = `<strong>Rating:</strong> ${anime.rating}`;
+
+    const popularity = document.createElement('p');
+    popularity.innerHTML = `<strong>Popularity:</strong> ${anime.popularity}`;
+
+    const genre = document.createElement('p');
+    genre.innerHTML = `<strong>Genre:</strong> ${anime.genre}`;
+
+    const episodes = document.createElement('p');
+    episodes.innerHTML = `<strong>Number of Episodes:</strong> ${anime.total_episodes}`;
+
+    // Create and append the button
+    const button = document.createElement('button');
+    button.className = buttonClass;
+    button.onclick = () => eval(buttonAction); // Dynamically call the add/remove function
+    button.innerText = buttonText;
+
+    // Append everything to the card
+    card.appendChild(title);
+    card.appendChild(img);
+    card.appendChild(rating);
+    card.appendChild(popularity);
+    card.appendChild(genre);
+    card.appendChild(episodes);
+    card.appendChild(button);
+
+    // Add the card to the container
+    container.appendChild(card);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  displayList();
+});
